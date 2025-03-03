29907266d3ed78dca76e6a3171c765ca8474d1b3
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-03 15:57:40 -0800

    Add solveRect function to calculate area and perimeter of a rectangle

--- node-example3.js
+++ node-example3.js
@@ -2,3 +2,18 @@ const rect = {
     perimeter: (x, y) => 2 * (x + y),
     area: (x, y) => x * y
 };
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with ${l} and ${w}`);
+    if (l <= 0 || w <= 0) {
+        console.log(`Rectangle dimensions should be greater than zero. values: ${l} ${w}`);
+    } else {
+        console.log(`Area is: ${rect.area(l, w)}`);
+        console.log(`Perimeter is: ${rect.perimeter(l, w)}`);
+    }
+}
+
+solveRect(6, 2);
+solveRect(8, 1);
+solveRect(0, 9);
+solveRect(4, -1);
